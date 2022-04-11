cbced7ae7788298a1e84618b8b9085751a804216
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-04-11 11:25:31 -0300

    Finished

--- Assets/Tema2.cs
+++ Assets/Tema2.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Tema2 : MonoBehaviour
 {
+    public string Nombre;
+    public int Año;
+    public string orientacion;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (Nombre == "")
+        {
+            Debug.Log("Error. Campo nombre vacío");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        else if (Año < 1 || Año > 5)
+        {
+            Debug.Log("Error. El año debe estar entre 1 y 5");
+        }
+
+        else if (Año < 3)
+        {
+            Debug.Log("Error. Aún estás en el ciclo básico");
+        }
+
+        else
+        {
+            switch (orientacion)
+            {
+                case "T":
+                    Debug.Log("Muchas gracias " + Nombre + "!");
+                    break;
+
+                case "D":
+                    Debug.Log("Muchas gracias " + Nombre + "!");
+                    break;
+
+                case "G":
+                    Debug.Log("Muchas gracias " + Nombre + "!");
+                    break;
+
+                case "M":
+                    Debug.Log("Muchas gracias " + Nombre + "!");
+                    break;
+
+                case "H":
+                    Debug.Log("Muchas gracias " + Nombre + "!");
+                    break;
+
+                default:
+                    Debug.Log("Solo puede ingresar T, D, G, M o H");
+                    break;
+            }
+        }        
     }
 }
